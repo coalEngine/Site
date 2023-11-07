b47392b849777c223bf9bd6d734b785dbf524d65
Thiery <weirdogamingidk@gmail.com>
2023-11-07 09:14:52 -0500

    Initial

---
+++ flower.js
@@ -0,0 +1,9 @@
+export default class Flower{
+    constructor(length, color, type){
+        this.length = length
+        this.color = color
+        this.type = type
+    }
+}
+
+export function grow(){}
---
+++ main.js
@@ -0,0 +1,9 @@
+
+import Flower from "./flower.js"
+const flower = new Flower
+
+function helloworld(){
+    console.log(flower)
+}
+
+helloworld()
